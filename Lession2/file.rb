421cd23a6e1ad629db726d173c2ecf2957a1b19e
Sayurov <me@sayurov.ru>
2022-12-13 21:45:10 +0300

    Ruletka 1.1

--- Lession2/file.rb
+++ Lession2/file.rb
@@ -1,8 +1,7 @@
 puts 'введите черное или красное'
 number_user = gets.strip
 random_number = ['черное','красное']
-puts(random_number.sample)
-if number_user == random_number
+if number_user == random_number.sample
     puts("Вы выиграли!")
 else
     puts("Вы проиграли!")
